4ab17f555c7d3f980ed3f8d99c8c7f86ed668da9
Muhammad Rana <136650096+muhammadrana411@users.noreply.github.com>
2024-01-21 16:03:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("password");
+const length = 16;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const symbols = "!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/~`";
+const numbers = "0123456789";
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+    
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copy(){
+    passwordBox.select();
+    document.execCommand("copy")
+}
